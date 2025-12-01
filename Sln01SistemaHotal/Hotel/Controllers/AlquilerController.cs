09ecda26693b2266c911e52a6c9ff4a75ea9bc80
RafaelCapristano <mamanirafael324@gmail.com>
2025-12-01 13:41:19 -0500

    Controller Alquiler

--- Sln01SistemaHotal/Hotel/Controllers/AlquilerController.cs
+++ Sln01SistemaHotal/Hotel/Controllers/AlquilerController.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Hotel.Data;
+using Hotel.Filtro;
+using Hotel.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Hotel.Data;
-using Hotel.ViewModels;
-using Hotel.Filtro;
+using Rotativa.AspNetCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Hotel.Controllers
 {
@@ -59,8 +60,40 @@ namespace Hotel.Controllers
                                       fechaSalida = a.fechaSalida,
                                       total = a.total,
                                   };
-            
+            ViewBag.Fecha1 = fecha1?.ToString("yyyy-MM-dd");
+            ViewBag.Fecha2 = fecha2?.ToString("yyyy-MM-dd");
             return View(await alquiler.ToListAsync());
         }
+        public async Task<IActionResult> ExportarPDF(DateTime? fecha1, DateTime? fecha2)
+        {
+
+            var alquiler = from a in _context.Alquiler
+                           join h in _context.Habitacion on a.idHab equals h.Id
+                           join c in _context.Cliente on a.idCli equals c.id
+                           join t in _context.TipoHab on h.TipoId equals t.id
+                           join td in _context.TipoDoc on c.tipoId equals td.id
+                           where a.fechaActual >= fecha1 && a.fechaActual <= fecha2
+                           select new ListaAlquilerFrontEnd
+                           {
+                               id = a.id,
+                               numHab = h.Numero,
+                               nombre = c.nombre + " " + c.apellido,
+                               documento = td.tipo + " " + c.numeroDoc,
+                               dias = a.dias,
+                               precio = t.precio,
+                               fechaActual = a.fechaActual,
+                               fechaEntrada = a.fechaEntrada,
+                               fechaSalida = a.fechaSalida,
+                               total = a.total,
+                           };
+
+            return new ViewAsPdf("ExportarPDF", await alquiler.ToListAsync())
+            {
+                FileName = "Reporte_Alquileres.pdf",
+            };
+        }
+
+
+
     }
 }
